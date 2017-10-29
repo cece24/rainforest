784a03c653fc328acd129e285fad62cda6101b05
Cecilia Wong <ceciliakhwong@gmail.com>
2017-10-29 19:22:57 -0400

    Add seeds to database using Faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+6.times do
+  Product.create!(
+    name: Faker::Commerce.product_name,
+    description: Faker::Lorem.paragraph(3),
+    price_in_cents: rand(100..10000)
+  )
+end
