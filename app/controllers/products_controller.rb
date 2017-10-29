5a7967a12521300b9bea360caf428ad1cfabd0db
Cecilia Wong <ceciliakhwong@gmail.com>
2017-10-29 19:06:34 -0400

    Generate Products controller with 7 actions

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,29 @@
+class ProductsController < ApplicationController
+  def index
+    #code
+  end
+
+  def new
+    #code
+  end
+
+  def create
+    #code
+  end
+
+  def show
+    #code
+  end
+
+  def edit
+
+  end
+
+  def update
+    #code
+  end
+
+  def delete
+    #code
+  end
+end
