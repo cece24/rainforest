008517091d9d510807711a736364502d682d9bd3
Cecilia Wong <ceciliakhwong@gmail.com>
2017-10-30 23:41:45 -0400

    Define Sessions create action, add links to layout to log in and out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,24 @@
 class SessionsController < ApplicationController
   def new
-    #code
+
   end
 
   def create
+    @user = User.find_by(email: params[:session][:email])
 
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      flash[:notice] = "Welcome back, #{ @user.email }!"
+      redirect_to root_url
+    else
+      flash[:alert] = "Sorry, an error was encountered while logging in!"
+      render :new
+    end
   end
 
   def destroy
-    #code
+    session[:user_id] = nil
+    flash[:notice] = "You have successfully logged out!"
+    redirect_to root_url
   end
 end
