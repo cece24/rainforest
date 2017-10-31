abf318e92a968bc2319a40f098081bb46e041e97
Cecilia Wong <ceciliakhwong@gmail.com>
2017-10-30 22:58:44 -0400

    Generate Users controller with new and create actions, generate routes for user

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "products#index"
 
+  resource :user 
+
   resources :products do
     resources :reviews, except: %i(index new show)
   end
