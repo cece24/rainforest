506856c07aa2de3437ab33855f927c156407b578
Cecilia Wong <ceciliakhwong@gmail.com>
2017-10-30 23:21:01 -0400

    Generate Sessions controller with new, create and destroy actions; define these routes only for sessions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,13 @@
+class SessionsController < ApplicationController
+  def new
+    #code
+  end
+
+  def create
+
+  end
+
+  def destroy
+    #code
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "products#index"
 
-  resource :users
+  resource :users, only: %i(new create)
+  resource :sessions, only: %i(new create destroy)
 
   resources :products do
     resources :reviews, except: %i(index new show)
