f770773765ab071901ff8e57be26d42112fede5e
Cecilia Wong <ceciliakhwong@gmail.com>
2017-10-30 23:17:16 -0400

    Define User controller new and create actions, create user new view, fix user routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,21 @@
 class UsersController < ApplicationController
   def new
-    #code
+    @user = User.new
   end
 
   def create
-    #code
+    @user = User.new(
+      email:    params[:user][:email],
+      password: params[:user][:password],
+      password_confirmation: params[:user][:password_confirmation]
+    )
+
+    if @user.save
+      flash[:notice] = "Your account has been successfully created!"
+      redirect_to root_url
+    else
+      flash[:alert] = "Sorry, an error was encountered while creating your account!"
+      render :new
+    end
   end
 end
